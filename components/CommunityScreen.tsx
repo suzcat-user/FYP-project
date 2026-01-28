38821d7de6f479087da420999f4729697576904b
23011376-Syarifah <23011376@myrp.edu.sg>
2026-01-28 21:03:04 +0800

    updated the leaderboard

--- components/CommunityScreen.tsx
+++ components/CommunityScreen.tsx
@@ -177,6 +177,44 @@ const CommunityScreen: React.FC<CommunityScreenProps> = ({ onRestart, scores, ho
 
       <div className="flex-1 overflow-hidden px-[4vmin] py-[4vmin] flex flex-col max-w-[1000px] mx-auto w-full z-10">
           <div className={`border-8 flex-1 flex flex-col transition-colors duration-500 ${isDarkMode ? 'border-indigo-900 bg-black/80 shadow-[0_0_40px_rgba(168,85,247,0.2)]' : 'border-sky-800 bg-sky-950/90'}`}>
+            {/* Podium for Top 3 */}
+            {!isLoadingLeaderboard && leaderboard && leaderboard.length > 0 && (
+              <div className="flex justify-center items-end gap-8 py-8">
+                {/* 2nd Place */}
+                {leaderboard[1] && (
+                  <div className="flex flex-col items-center">
+                    <div className="bg-gray-400/80 rounded-t-lg rounded-b-sm w-20 h-32 flex flex-col justify-end items-center shadow-lg border-b-4 border-gray-500">
+                      <span className="text-4xl mb-2">🥈</span>
+                      <span className="font-press-start text-[1.2vmin] text-white mb-2">{leaderboard[1].username}</span>
+                      <span className="font-press-start text-[1.1vmin] text-cyan-200 mb-4">{leaderboard[1].score} XP</span>
+                    </div>
+                    <span className="mt-2 font-press-start text-[1vmin] text-gray-300">2nd</span>
+                  </div>
+                )}
+                {/* 1st Place */}
+                {leaderboard[0] && (
+                  <div className="flex flex-col items-center">
+                    <div className="bg-yellow-400/90 rounded-t-lg rounded-b-sm w-24 h-40 flex flex-col justify-end items-center shadow-xl border-b-8 border-yellow-600">
+                      <span className="text-5xl mb-2">🥇</span>
+                      <span className="font-press-start text-[1.5vmin] text-black mb-2">{leaderboard[0].username}</span>
+                      <span className="font-press-start text-[1.2vmin] text-sky-900 mb-4">{leaderboard[0].score} XP</span>
+                    </div>
+                    <span className="mt-2 font-press-start text-[1.1vmin] text-yellow-300">1st</span>
+                  </div>
+                )}
+                {/* 3rd Place */}
+                {leaderboard[2] && (
+                  <div className="flex flex-col items-center">
+                    <div className="bg-orange-400/80 rounded-t-lg rounded-b-sm w-20 h-28 flex flex-col justify-end items-center shadow-lg border-b-4 border-orange-600">
+                      <span className="text-4xl mb-2">🥉</span>
+                      <span className="font-press-start text-[1.2vmin] text-white mb-2">{leaderboard[2].username}</span>
+                      <span className="font-press-start text-[1.1vmin] text-cyan-200 mb-4">{leaderboard[2].score} XP</span>
+                    </div>
+                    <span className="mt-2 font-press-start text-[1vmin] text-orange-200">3rd</span>
+                  </div>
+                )}
+              </div>
+            )}
               
               <div className="flex p-4 font-press-start text-[1.5vmin] border-b-8 border-current bg-white/5">
                   <div className="w-[15%] text-center">RANK</div>
